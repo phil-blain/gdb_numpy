c04058b3e83b73a4a3c40598d9ca5dc769f2554a
Philippe Blain <levraiphilippeblain@gmail.com>
2021-02-10 17:40:02 -0500

    Run black

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
-setup(name = 'gdb_numpy',
-      version = '1.0',
-      description = 'Manupulating matrices and vectors in gdb.',
-      author = 'Man Yue Mo',
-      author_email = 'manyue.mo@gmail.com',
-      license = 'CPOL', 
-      py_modules = ['gdb_numpy','deref']
-      )
+
+setup(
+    name="gdb_numpy",
+    version="1.0",
+    description="Manupulating matrices and vectors in gdb.",
+    author="Man Yue Mo",
+    author_email="manyue.mo@gmail.com",
+    license="CPOL",
+    py_modules=["gdb_numpy", "deref"],
+)
